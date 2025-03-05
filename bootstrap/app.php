82ca409e3b1ff7b52fe5077236444017574a46e1
alandotlbrt <alanlebrument@icloud.com>
2025-03-05 13:22:04 +0100

    fix(deploy): rm github folder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,3 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
+app()->usePublicPath(base_path('public_html'));
+return $app;
